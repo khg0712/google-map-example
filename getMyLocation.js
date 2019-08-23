25dbad6e17fedba055fc605e46f4de0c1b415518
김형규 <k7120792@gmail.com>
2019-08-23 14:58:28 +0900

    Add getMyLocation

---
+++ getMyLocation.js
@@ -0,0 +1,19 @@
+function getLocation() {
+  return new Promise((resolve, reject) => {
+    if (!navigator.geolocation) reject('geolocation is unusable');
+
+    function success(position) {
+      const { latitude, longitude } = position.coords;
+      resolve({
+        latitude,
+        longitude
+      });
+    }
+
+    function error(err) {
+      reject(err);
+    }
+
+    navigator.geolocation.getCurrentPosition(success, error);
+  });
+}
