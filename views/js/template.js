3661f35017cb019a5d43ae7dbd2c826def49eb44
김형규 <k7120792@gmail.com>
2019-09-05 22:23:26 +0900

    Modify template

--- views/js/template.js
+++ views/js/template.js
@@ -1,20 +1,28 @@
 (function(exports){
   'use strict';
   function Template(){
-    console.log('template created')
     this.defaultTemplate = `
       <li class="parking-record">
-        <p class="place">{{place}}</p>
-        <p class="address">{{address}}</p>
-        <p class="date">{{date}}</p>
+        <p class="index">
+          {{index}}
+        </p>
+        <div class="info">
+          <p class="address">{{address}}</p>
+          <p class="date">{{date}}</p>
+        </div>
+        <button class="delete-btn">
+          <i class="material-icons">
+            close
+          </i>
+        </button>
       </li>
     `;
     this.insert = function (data) {
       console.log('template insert');
       let view = '';
-      for(var i = 0; i < data.length; i++){
+      for(var i=data.length-1; i>=0; i--){
         let template = this.defaultTemplate;
-        template = template.replace('{{place}}', data[i].place);
+        template = template.replace('{{index}}', i+1);
         template = template.replace('{{address}}', data[i].address);
         template = template.replace('{{date}}', data[i].date);
         view += template;
