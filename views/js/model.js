3f72e51c9e2e84851d0a54ca80d01588e8a2b3c2
김형규 <k7120792@gmail.com>
2019-09-05 22:23:36 +0900

    Modify model

--- views/js/model.js
+++ views/js/model.js
@@ -1,20 +1,39 @@
 (function(exports){
   'use strict';
   function Model(storage){
-    console.log('Model created!');
     this.storage = storage;
   }
 
-  Model.prototype.create = function(data, callback){
+  Model.prototype.create = function(data){
     console.log('Model.create method execute!');
-    callback = callback || function () {}; 
-
-    this.storage.save(data, callback);
+    return fetch('http://127.0.0.1:3000/park', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data),
+    })
   };
 
   Model.prototype.read = function(callback){
     return this.storage.findAll(callback);
   };
+
+  Model.prototype.fetch = function() {
+    return fetch('http://127.0.0.1:3000/park')
+  }
+
+  Model.prototype.delete = function(data) {
+    return fetch('http://127.0.0.1:3000/park', {
+      method: 'DELETE',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data),
+    })
+  }
 
   exports.app = exports.app || {};
   exports.app.Model = Model;
